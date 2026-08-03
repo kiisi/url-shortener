b6ebbffff13504f90552711d31e69bdde9dc769d
kiisi <kiisifelix06@gmail.com>
2026-08-03 14:55:50 +0100

    feat: restoration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { sourGummy } from "./fonts";
+import { cn } from "../utils";
+import Navbar from "./components/layouts/Navbar";
+import Hero from "./components/home/Hero";
+import Footer from "./components/layouts/Footer";
+import Features from "./components/home/Features";
+import FAQs from "./components/home/Faqs";
+
 
 export default function Home() {
-  redirect("/dashboard");
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      {/* RECENT LINKS */}
+      <div className="w-full max-w-[1200px] mx-auto -pt-[80px] pb-[40px] px-6">
+        <div></div>
+      </div>
+      <Features />
+      <FAQs />
+      <Footer />
+    </>
+  );
 }
